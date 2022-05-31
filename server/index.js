077e55dc5a54734441a7fa95961a766984f04f85
Viachaslau Abaimau <w.abaimov@gmail.com>
2022-05-31 18:12:07 +0300

    init

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+const port = process.env.PORT || "3001";
+
+// app.use(express.static(path.join(__dirname, "build")));
+
+app.get("/api/products", function (req, res) {
+  res.status(200).json([
+    {
+      id: 1,
+      name: "Nokia",
+    },
+    {
+      id: 2,
+      name: "Motorola",
+    },
+  ]);
+});
+
+app.listen(port);
